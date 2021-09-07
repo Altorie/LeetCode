51cc526cd4f924a2a2c68a0847357c7f979b4f33
Altorie <735958089@qq.com>
2021-09-07 09:59:18 +0800

    力扣45：跳跃游戏2

--- DP/src/main/java/com/sjy/easy/Day4.java
+++ DP/src/main/java/com/sjy/easy/Day4.java
@@ -3,7 +3,7 @@ package com.sjy.easy;
 public class Day4 {
     public static void main(String[] args) {
         Day4 day4 = new Day4();
-        System.out.println(day4.canJump(new int[]{1,2}));
+        System.out.println(day4.jump(new int[]{2,3,1,1,4}));
     }
 
     /**
@@ -36,6 +36,41 @@ public class Day4 {
             }
         }
         return true;
+    }
+
+    /**
+     * 给你一个非负整数数组nums ，你最初位于数组的第一个位置。
+     * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+     * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+     * 假设你总是可以到达数组的最后一个位置。
+     *
+     * 输入: nums = [2,3,1,1,4]
+     * 输出: 2
+     * 解释: 跳到最后一个位置的最小跳跃数是 2。
+     *     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+
+     * @param nums
+     * @return
+     */
+    public int jump(int[] nums) {
+        int len = nums.length;
+        if (len == 1 || len == 2){
+            return len - 1;
+        }
+        int[] dp = new int[len];
+        dp[0] = 0;
+        for (int i = 0; i < len-1; i++) {
+            int r = Integer.min(i+nums[i], len-1);
+            for (int j = i+1; j <= r; j++) {
+                if (dp[j]==0){
+                    dp[j] = dp[i]+1;
+                }
+                if (j == len-1){
+                    return dp[j];
+                }
+            }
+        }
+        return dp[len-1];
     }
     
 }
