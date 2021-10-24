b43a9ee567c265618b2ec30707db8b82ddd21a5d
Altorie <735958089@qq.com>
2021-10-24 19:18:17 +0800

    堆的应用：找最大的 K 个数、找第 K 大的数等

---
+++ DS/src/main/java/com/sjy/heap/HeapApplications.java
@@ -0,0 +1,28 @@
+package com.sjy.heap;
+
+/**
+ * 堆的一些应用
+ */
+public class HeapApplications {
+
+    /**
+     * 找到数据流中最大的K个数
+     *
+     * 基本思路：
+     *   开辟一个大小为 k 的结构。
+     *   遍历数组，将每一个值与结构中储存的最小值比较，如果大于最小值就进行替换
+     *   这个数据结构可以采用 小顶堆实现
+     * @param nums
+     * @return
+     */
+    private int[] findLargestKs(int[] nums, int k){
+        for(int value: nums){
+
+        }
+        return null;
+    }
+
+    private int findK(){
+        return 0;
+    }
+}
