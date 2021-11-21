1ab8705b3cc39108f954b9312cde3d411f27af06
Altorie <735958089@qq.com>
2021-11-21 14:25:26 +0800

    剑指 Offer II 042. 最近请求次数

---
+++ JianZhiOffer/src/main/java/version/two/RecentCounter.java
@@ -0,0 +1,45 @@
+package version.two;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ * @AUTHOR zuo-zhenjun
+ * @TIME 2021/11/21 13:02
+ * @DESCRIPTION
+ *      剑指 Offer II 042. 最近请求次数
+ **/
+public class RecentCounter {
+    Deque<Integer> queue = new ArrayDeque<>();
+    public RecentCounter() {
+
+    }
+
+    public int ping(int t) {
+        queue.addLast(t);
+        while (queue.getFirst() < t-3000){
+            queue.removeFirst();
+        }
+        return queue.size();
+    }
+}
