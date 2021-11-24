7de2a3da8aea73f731cb28be0dfca3af82526b64
Altorie <735958089@qq.com>
2021-11-24 16:10:02 +0800

    剑指 Offer II 031. 最近最少使用缓存

--- JianZhiOffer/src/main/java/version/two/LRUCache.java
+++ JianZhiOffer/src/main/java/version/two/LRUCache.java
@@ -33,8 +33,8 @@ import java.util.Map;
 public class LRUCache {
     private int capacity;
     private int size;
-    private LRUNode dumpy = new LRUNode(0,0); // 虚拟头结点
-    private Map<Integer, Integer> map = new HashMap<>();
+    private LRUNode head;
+    private Map<Integer, LRUNode> map = new HashMap<>();
     public LRUCache(int capacity) {
         this.capacity = capacity;
     }
@@ -45,18 +45,24 @@ public class LRUCache {
      * @return
      */
     public int get(int key) {
-        if (map.getOrDefault(key, 0) == 0){ // 不存在
+        if (map.getOrDefault(key, null) == null){ // 不存在
             return -1;
         }
-        LRUNode p = dumpy;
-        while (p.next.key!=key){
-            p = p.next;
+        // 存在
+        LRUNode node = map.get(key);
+        if (node != head){ // 如果 get 的节点不是头结点，需要将 node 的位置调整到 head
+            // 把 node 删除
+            node.pre.next = node.next;
+            node.next.pre = node.pre;
+            // 把 node 插入 tail 和 head 之间
+            LRUNode tail = head.pre;
+            tail.next = node;
+            node.next = head;
+            head.pre = node;
+            node.pre = tail;
+            head = node;
         }
-        LRUNode target = p.next;
-        p.next = target.next;
-        target.next = dumpy.next;
-        dumpy.next = target;
-        return target.value;
+        return head.value;
     }
 
     /**
@@ -66,43 +72,59 @@ public class LRUCache {
      * @param value
      */
     public void put(int key, int value) {
-        if ( size == 0 || map.getOrDefault(key, 0) == 0){ // 关键字不存在
+        if ( map.getOrDefault(key, null) == null){ // 关键字不存在
+            // 创建节点对象
             LRUNode node = new LRUNode(key, value);
-            node.next = dumpy.next;
-            dumpy.next = node;
-            map.put(key, 1);
+            if (size == 0){
+                head = node;
+                head.pre = head;
+                head.next = head;
+            }
+            // 将节点插入链表头
+            node.pre = head.pre;
+            head.pre = node;
+            node.pre.next = node;
+            node.next = head;
+            head = node;
+            // map中记录
+            map.put(key, node);
             if (size < capacity){
                 size++;
             } else { // 把最后一个删除
-                LRUNode p = dumpy;
-                while (p.next.next!=null){
-                    p = p.next;
-                }
-                map.put(p.next.key, 0);
-                p.next = null;
+                // 在 map 中删除
+                map.put(head.pre.key, null);
+                // 在链表中删除
+                head.pre = head.pre.pre;
+                head.pre.next = head;
             }
         } else {
             get(key);
-            dumpy.next.value = value;
+            head.value = value;
         }
     }
 
     public static void main(String[] args) {
-        LRUCache lRUCache = new LRUCache(2);
+        LRUCache lRUCache = new LRUCache(3);
         lRUCache.put(1, 1); // 缓存是 {1=1}
-        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
-        System.out.println(lRUCache.get(1));
-        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        lRUCache.put(2, 2); // 缓存是 {2=2, 1=1}
+        lRUCache.put(3, 3); // 缓存是 {3=3, 2=2, 1=1}
+        lRUCache.put(4, 4); // 缓存是 {4=4, 3=3, 2=2}
+        System.out.println(lRUCache.get(4));
+        System.out.println(lRUCache.get(3));
         System.out.println(lRUCache.get(2));
-        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
         System.out.println(lRUCache.get(1));
+        lRUCache.put(5, 5);
+        System.out.println(lRUCache.get(1));
+        System.out.println(lRUCache.get(2));
         System.out.println(lRUCache.get(3));
         System.out.println(lRUCache.get(4));
+        System.out.println(lRUCache.get(5));
     }
 }
 class LRUNode{
     int key;
     int value;
+    LRUNode pre;
     LRUNode next;
 
     public LRUNode(int key, int value) {
